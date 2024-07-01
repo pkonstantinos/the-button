d0ead7cba015ed353d6539171a0664da457f8ccc
Papoulidis Konstantinos <papoulidis.kosta@gmail.com>
2024-07-01 11:31:05 +0200

    Create Spring Project

---
+++ src/main/java/hu/kine126/the_button/TheButtonApplication.java
@@ -0,0 +1,13 @@
+package hu.kine126.the_button;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheButtonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheButtonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/kine126/the_button/TheButtonApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.kine126.the_button;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheButtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
